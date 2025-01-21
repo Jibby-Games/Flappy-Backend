f66748455e89aa1143a87294f181a9361b8964e2
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2025-01-21 22:28:45 +0000

    chore: update manager deps

--- test_manager_request.sh
+++ test_manager_request.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # POST request to start a server
 curl -H 'Content-Type: application/json' \
-    -d '{ "name":"test-server","list":1, "version": "0.12.0"}' \
+    -d '{ "name":"test-server","list":1, "version": "0.13.0"}' \
     -X POST -k \
     https://localhost/api/manager/request
